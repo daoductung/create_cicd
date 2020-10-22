13b7a93f147175ca27cd36b571dacb861f1198cc
tungdd <tungdd@vega.com.vn>
2020-10-22 09:16:32 +0700

    test

--- bin/run_api.sh
+++ bin/run_api.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export APP_PID=$!;
-echo  {$APP_PID};
+export APP_PID=$!
+echo  {$APP_PID}
 if [[ "" !=  "$APP_PID" ]]; then
   echo "killing $APP_PID"
   kill -9 $APP_PID
 fi
-sudo python main.py >> log.txt 2>&1 &;
+sudo python main.py >> log.txt 2>&1 &
 
