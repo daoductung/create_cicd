829962d69743e597798d38c96ef2e4fc07a8db68
tungdd <tungdd@vega.com.vn>
2020-10-22 11:31:19 +0700

    'update'

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ def home():
     return "Hello Tung Update!"
 
 
+@app.route('/test')
+def test():
+    return "Update thanh cong r ne!"
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9799)
