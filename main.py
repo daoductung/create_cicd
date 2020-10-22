33a10d3ba8474311abc08b39cd08968346290911
tungdd <tungdd@vega.com.vn>
2020-10-22 10:18:05 +0700

    add

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 from flask import Flask
-from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return "<h1>Hello!</h1>"
+@app.route('/')
+def home():
+    return "Hello World!"
 
 
-FLASK_RUN_HOST = '0.0.0.0'
-FLASK_RUN_PORT = 9797
-
-http_server = WSGIServer((str(FLASK_RUN_HOST), int(FLASK_RUN_PORT)), app)
-http_server.serve_forever()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=9799)
