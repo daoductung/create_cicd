b3045bcd10a31921932168cfd8b243d1551533b5
Đào Đức Tùng <36580788+daoductung@users.noreply.github.com>
2021-01-20 10:27:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,62 @@
+from __future__ import print_function
+import pickle
+import os.path
 from flask import Flask
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
 app = Flask(__name__)
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '11oKN8trdZSEAiE4T-7TVsf17K0qFQ8uk05oHSFUDm50'
+SAMPLE_RANGE_NAME = 'DATABASE_KICKOFF'
 
 
-@app.route('/')
+@app.route('/home')
 def home():
-    return "Hello, World!"
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values()
+    result = result.get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME).execute()
+
+    values = result.get('values', [])
+
+    if not values:
+        return 'No data found.'
+    else:
+        for row in values:
+            if not row:
+                break
+            # Print columns A and E, which correspond to indices 0 and 4.
+            return ('%s, %s' % (row[0], row[3]))
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=9799)
+if __name__ == '__main__':
+    app.run(port=9088, debug=True)
